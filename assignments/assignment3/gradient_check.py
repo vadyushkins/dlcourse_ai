b63dfe724cd4325ddfd45c42bbc02435e5a81b0f
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2021-03-24 14:27:37 +0300

    Assignment3 updated gradient_check.py

--- assignments/assignment3/gradient_check.py
+++ assignments/assignment3/gradient_check.py
@@ -12,29 +12,32 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
       delta: step to compute numerical gradient
       tol: tolerance for comparing numerical and analytical gradient
 
-    Return:
-      bool indicating whether gradients match or not
+    Return: bool indicating whether gradients match or not
     """
+
     assert isinstance(x, np.ndarray)
     assert x.dtype == np.float
 
+    orig_x = x.copy()
     fx, analytic_grad = f(x)
-    analytic_grad = analytic_grad.copy()
+    assert np.all(np.isclose(orig_x, x, tol)), "Functions shouldn't modify input variables"
 
     assert analytic_grad.shape == x.shape
+    analytic_grad = analytic_grad.copy()
 
+    # We will go through every dimension of x and compute numeric derivative for it
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
 
-        # TODO Copy from previous assignment
-        raise Exception("Not implemented!")
+        ix_delta = np.zeros_like(x)
+        ix_delta[ix] = delta
+        numeric_grad_at_ix = (f(x + ix_delta)[0] - f(x - ix_delta)[0]) / (2 * delta)
 
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
-                  ix, analytic_grad_at_ix, numeric_grad_at_ix))
+                ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
 
         it.iternext()
